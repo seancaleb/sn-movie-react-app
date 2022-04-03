02701e02a2728f75427b4d4746abc5e64e3ee76b
Sean Caleb <snclbmnzubs@gmail.com>
2022-04-04 03:42:07 +0800

    remove window.load listener

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { Box, useBoolean } from "@chakra-ui/react";
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useMemo, useState } from "react";
 import Routes from "./routes/Routes";
 // import { useDispatch } from "react-redux";
 
@@ -26,29 +25,10 @@ const App = () => {
   //   dispatch(fetchMovieGenres());
   // }, []);
 
-  const [isMounted, setIsMounted] = useBoolean();
-
-  useEffect(() => {
-    const handler = () => setIsMounted.on();
-
-    if (document.readyState === "complete") {
-      handler();
-    } else {
-      window.addEventListener("load", handler);
-      return () => window.removeEventListener("load", handler);
-    }
-  }, []);
-
   return (
-    <>
-      {isMounted ? (
-        <TogglerProvider>
-          <Routes />
-        </TogglerProvider>
-      ) : (
-        <Box minH="100vh" bg="brand.dark.primary" w="100%" />
-      )}
-    </>
+    <TogglerProvider>
+      <Routes />
+    </TogglerProvider>
   );
 };
 
