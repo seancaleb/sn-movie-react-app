eec583db85df13697b4a8d7eaa25a8bee5434101
Sean Caleb <snclbmnzubs@gmail.com>
2022-04-04 03:35:32 +0800

    move window.load listener

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,14 +1,35 @@
+import { Box, useBoolean } from "@chakra-ui/react";
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = (props) => {
   const location = useLocation();
+  const [isMounted, setIsMounted] = useBoolean();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
 
-  return <React.Fragment {...props} />;
+  useEffect(() => {
+    const handler = () => setIsMounted.on();
+
+    if (document.readyState === "complete") {
+      handler();
+    } else {
+      window.addEventListener("load", handler);
+      return () => window.removeEventListener("load", handler);
+    }
+  }, [location, setIsMounted]);
+
+  return (
+    <>
+      {!isMounted ? (
+        <Box minH="100vh" bg="brand.dark.primary" w="100%" />
+      ) : (
+        <React.Fragment {...props} />
+      )}
+    </>
+  );
 };
 
 export default ScrollToTop;
