f7b3a1d4b8ac08e5c1ca259adc444406ae5e489f
Sean Caleb <snclbmnzubs@gmail.com>
2022-04-04 17:38:28 +0800

    add selectGenre selector in slice

--- src/features/genres/genresSlice.js
+++ src/features/genres/genresSlice.js
@@ -12,17 +12,14 @@ const key = `${import.meta.env.VITE_TMDB_API_KEY}`;
 
 const url = `${baseURL}/genre/movie/list?api_key=${key}`;
 
-export const fetchMovieGenres = createAsyncThunk(
-  "genres/fetchMovieGenres",
-  async (thunkAPI) => {
-    try {
-      const response = await axios.get(url);
-      return response.data.genres;
-    } catch (err) {
-      // NEED TO CONFIGURE ERROR RESPONSE !!!!
-    }
+export const fetchMovieGenres = createAsyncThunk("genres/fetchMovieGenres", async (thunkAPI) => {
+  try {
+    const response = await axios.get(url);
+    return response.data.genres;
+  } catch (err) {
+    // NEED TO CONFIGURE ERROR RESPONSE !!!!
   }
-);
+});
 
 const options = {
   name: "genres",
@@ -45,6 +42,7 @@ const options = {
 };
 
 export const selectGenres = (state) => state.genres.entities;
+export const selectGenre = (state, id) => state.genres.entities.filter((genre) => id === genre.id);
 
 const genresSlice = createSlice(options);
 export default genresSlice.reducer;
