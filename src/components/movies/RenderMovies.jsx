c9499430f1a451539b6251ca0c0d3b37b021846e
Sean Caleb <snclbmnzubs@gmail.com>
2022-04-04 09:28:24 +0800

    remove condition to check for cast property

--- src/components/movies/RenderMovies.jsx
+++ src/components/movies/RenderMovies.jsx
@@ -6,15 +6,12 @@ import MoviesFallback from "./MoviesFallback";
 const RenderMovies = ({ data, error, isFetching, isSuccess, isError, limit }) => {
   if (isFetching) return <MoviesFallback isFetching={isFetching} limit={limit} />;
   else if (isSuccess)
-    if (data.results) {
-      return data.results.length !== 0 ? (
-        <MoviesDataView data={data.results} />
-      ) : (
-        <Text color="whiteAlpha.600">None specified yet.</Text>
-      );
-    } else if (data.cast) {
-      return <MoviesDataView data={data.cast} />;
-    } else if (isError) throw error;
+    return data.results.length !== 0 ? (
+      <MoviesDataView data={data.results} />
+    ) : (
+      <Text color="whiteAlpha.600">None specified yet.</Text>
+    );
+  else if (isError) throw error;
 };
 
 export default React.memo(RenderMovies);
