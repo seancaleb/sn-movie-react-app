66040adb83e53d123115ff9de0c90f3c7cca8c63
Sean Caleb <snclbmnzubs@gmail.com>
2022-04-07 01:37:49 +0800

    add an outside alerter event hook

---
+++ src/components/hooks/useOutsideAlerter.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+const useOutsideAlerter = (ref, fn) => {
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (ref.current && !ref.current.contains(event.target)) {
+        fn();
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref]);
+};
+
+export default useOutsideAlerter;
