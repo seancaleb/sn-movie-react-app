a821df6c884c0657559a025abe1dfe5cd7c7a69a
Sean Caleb <snclbmnzubs@gmail.com>
2022-08-07 18:06:37 +0800

    lazy load route components

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes as MyRoutes } from "react-router-dom";
-import { Footer, Header, BaseTemplate, ScrollToTop } from "../components";
-import Cast from "../components/cast/Cast";
-import Genre from "../components/genre/Genre";
-import Movie from "../components/movie/Movie";
-import MoviesDataView from "../components/movies/MoviesDataView";
-import PeopleDataView from "../components/people/PeopleDataView";
+import { Footer, Header, ScrollToTop, Preloader } from "../components";
 import { popularArgs, topratedArgs, upcomingArgs } from "../data";
 import {
   useGetMoviesQuery,
@@ -12,9 +7,16 @@ import {
   useGetSearchByCategoryQuery,
 } from "../features/api/moviesSlice";
 import Home from "../pages/Home";
-
 import { useSelector } from "react-redux";
 import { selectQuery } from "../features/query/querySlice";
+import { lazy, Suspense } from "react";
+
+const MoviesDataView = lazy(() => import("../components/movies/MoviesDataView"));
+const PeopleDataView = lazy(() => import("../components/people/PeopleDataView"));
+const BaseTemplate = lazy(() => import("../components/BaseTemplate"));
+const Cast = lazy(() => import("../components/cast/Cast"));
+const Genre = lazy(() => import("../components/genre/Genre"));
+const Movie = lazy(() => import("../components/movie/Movie"));
 
 const Routes = () => {
   const { value } = useSelector(selectQuery);
@@ -23,103 +25,105 @@ const Routes = () => {
     <BrowserRouter>
       <ScrollToTop>
         <Header />
-        <MyRoutes>
-          <Route exact path="/" element={<Home />} />
-          <Route
-            exact
-            path="/movie/popular"
-            element={
-              <BaseTemplate
-                title="Popular Movies"
-                fnArgs={popularArgs}
-                pagination
-                fn={useGetMoviesQuery}
-                component={(data, isFetching) => (
-                  <MoviesDataView data={data.results} isFetching={isFetching} />
-                )}
-              />
-            }
-          />
-          <Route
-            exact
-            path="/movie/top_rated"
-            element={
-              <BaseTemplate
-                title="Top Rated Movies"
-                fnArgs={topratedArgs}
-                pagination
-                fn={useGetMoviesQuery}
-                component={(data, isFetching) => (
-                  <MoviesDataView data={data.results} isFetching={isFetching} />
-                )}
-              />
-            }
-          />
-          <Route
-            exact
-            path="/movie/upcoming"
-            element={
-              <BaseTemplate
-                title="Upcoming Movies"
-                fnArgs={upcomingArgs}
-                pagination
-                fn={useGetMoviesQuery}
-                component={(data, isFetching) => (
-                  <MoviesDataView data={data.results} isFetching={isFetching} />
-                )}
-              />
-            }
-          />
-          <Route
-            exact
-            path="/person/popular"
-            element={
-              <BaseTemplate
-                title="Popular Celebrities"
-                pagination
-                fn={useGetPopularPeopleQuery}
-                component={(data, isFetching) => (
-                  <PeopleDataView data={data.results} isFetching={isFetching} />
-                )}
-              />
-            }
-          />
-          <Route exact path="/discover/movie/:genreId" element={<Genre />} />
-          <Route exact path="/cast/:castId" element={<Cast />} />
-          <Route exact path="/movie/:movieId" element={<Movie />} />
-          <Route exact path="/movie" element={<Navigate to="/" replace />} />
-          <Route
-            exact
-            path={`search/movie/${value}`}
-            element={
-              <BaseTemplate
-                title={`Search results for '${value}'`}
-                pagination
-                fnArgs={{ category: "movie", query: value }}
-                fn={useGetSearchByCategoryQuery}
-                component={(data, isFetching) => (
-                  <MoviesDataView data={data.results} isFetching={isFetching} />
-                )}
-              />
-            }
-          />
-          <Route
-            exact
-            path={`search/person/${value}`}
-            element={
-              <BaseTemplate
-                title={`Search results for '${value}'`}
-                pagination
-                fnArgs={{ category: "person", query: value }}
-                fn={useGetSearchByCategoryQuery}
-                component={(data, isFetching) => (
-                  <PeopleDataView data={data.results} isFetching={isFetching} />
-                )}
-              />
-            }
-          />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </MyRoutes>
+        <Suspense fallback={<Preloader />}>
+          <MyRoutes>
+            <Route exact path="/" element={<Home />} />
+            <Route
+              exact
+              path="/movie/popular"
+              element={
+                <BaseTemplate
+                  title="Popular Movies"
+                  fnArgs={popularArgs}
+                  pagination
+                  fn={useGetMoviesQuery}
+                  component={(data, isFetching) => (
+                    <MoviesDataView data={data.results} isFetching={isFetching} />
+                  )}
+                />
+              }
+            />
+            <Route
+              exact
+              path="/movie/top_rated"
+              element={
+                <BaseTemplate
+                  title="Top Rated Movies"
+                  fnArgs={topratedArgs}
+                  pagination
+                  fn={useGetMoviesQuery}
+                  component={(data, isFetching) => (
+                    <MoviesDataView data={data.results} isFetching={isFetching} />
+                  )}
+                />
+              }
+            />
+            <Route
+              exact
+              path="/movie/upcoming"
+              element={
+                <BaseTemplate
+                  title="Upcoming Movies"
+                  fnArgs={upcomingArgs}
+                  pagination
+                  fn={useGetMoviesQuery}
+                  component={(data, isFetching) => (
+                    <MoviesDataView data={data.results} isFetching={isFetching} />
+                  )}
+                />
+              }
+            />
+            <Route
+              exact
+              path="/person/popular"
+              element={
+                <BaseTemplate
+                  title="Popular Celebrities"
+                  pagination
+                  fn={useGetPopularPeopleQuery}
+                  component={(data, isFetching) => (
+                    <PeopleDataView data={data.results} isFetching={isFetching} />
+                  )}
+                />
+              }
+            />
+            <Route exact path="/discover/movie/:genreId" element={<Genre />} />
+            <Route exact path="/cast/:castId" element={<Cast />} />
+            <Route exact path="/movie/:movieId" element={<Movie />} />
+            <Route exact path="/movie" element={<Navigate to="/" replace />} />
+            <Route
+              exact
+              path={`search/movie/${value}`}
+              element={
+                <BaseTemplate
+                  title={`Search results for '${value}'`}
+                  pagination
+                  fnArgs={{ category: "movie", query: value }}
+                  fn={useGetSearchByCategoryQuery}
+                  component={(data, isFetching) => (
+                    <MoviesDataView data={data.results} isFetching={isFetching} />
+                  )}
+                />
+              }
+            />
+            <Route
+              exact
+              path={`search/person/${value}`}
+              element={
+                <BaseTemplate
+                  title={`Search results for '${value}'`}
+                  pagination
+                  fnArgs={{ category: "person", query: value }}
+                  fn={useGetSearchByCategoryQuery}
+                  component={(data, isFetching) => (
+                    <PeopleDataView data={data.results} isFetching={isFetching} />
+                  )}
+                />
+              }
+            />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </MyRoutes>
+        </Suspense>
         <Footer />
       </ScrollToTop>
     </BrowserRouter>
